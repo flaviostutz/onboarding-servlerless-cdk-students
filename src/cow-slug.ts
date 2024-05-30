e293946030140596cba6080e1126a52132c6875f
MoisesFGNeto <117291517+MoisesFGNeto@users.noreply.github.com>
2024-05-30 16:59:38 +0200

    updating dependecies and adjusting code according to linting errors

--- src/cow-slug.ts
+++ src/cow-slug.ts
@@ -1,6 +1,10 @@
 import * as cowsay from 'cowsay';
 import { generateSlug } from 'random-word-slugs';
 
-let output = cowsay.say({ text: generateSlug()});
+const output = cowsay.say({ text: generateSlug() });
 
-console.log(output);
+function showOutput(): string {
+  return output;
+}
+
+showOutput();
--- src/slug.test.ts
+++ src/slug.test.ts
@@ -1,8 +1,8 @@
 // Import the function you want to test
-import { generateRandomSlug } from "./slug";
+import { generateRandomSlug } from './slug';
 
-describe("generatesRandomSlug", () => {
-  it("should generate an array of slugs with the specified count", () => {
+describe('generatesRandomSlug', () => {
+  it('should generate an array of slugs with the specified count', () => {
     const count = 10;
     const slugs = generateRandomSlug(count);
     // Check if slugs is an array
@@ -10,7 +10,7 @@ describe("generatesRandomSlug", () => {
     // Check if slugs has the correct length
     expect(slugs.length).toBe(count);
     // Check if all elements in slugs are strings
-    slugs.forEach(slug => {
+    slugs.forEach((slug) => {
       expect(typeof slug).toBe('string');
     });
   });
