926992700a02812c996fa90a68a5d0eb7203288a
Moisés Neto <117291517+MoisesFGNeto@users.noreply.github.com>
2024-09-29 00:08:37 +0200

    Revert "Task 12 - Github actions created"

--- cdk/bin/cdk.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-
-import { MyApiStack } from '../lib/cdk-stack';
-
-const app = new cdk.App();
-
-// eslint-disable-next-line no-new
-new MyApiStack(app, 'MyApiStack');
