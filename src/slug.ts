2d270eaf25a5d65a76d73b61049f81e659cf08d6
MoisesFGNeto <117291517+MoisesFGNeto@users.noreply.github.com>
2024-10-06 23:34:56 +0200

    remving /src

--- src/slug.ts
+++
@@ -1,9 +0,0 @@
-import { generateSlug } from 'random-word-slugs';
-
-export function generateRandomSlug(count: number): string[] {
-  const slugs: string[] = [];
-  for (let i = 0; i < count; i += 1) {
-    slugs.push(generateSlug());
-  }
-  return slugs;
-}
